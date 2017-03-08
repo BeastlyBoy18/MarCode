16b731f14c72ffbf38c87c8904717643dccd540f
game110pm <game110pm@605-110-STU03>
2017-03-08 12:46:16 -0700

    Bcounter

---
+++ B-Counter/B-Counter/Source.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int bcounter = 0;
+	char input = 'b';
+
+	while (input == 'b') {
+		cin >> input;
+		if (input == 'b') 
+			bcounter = bcounter + 1;
+		else if (input != 'b') {
+			cout << bcounter << endl;
+		
+		}
+		
+		
+	
+	
+	
+	} // While Loop
+	}// Main
+
+
+
